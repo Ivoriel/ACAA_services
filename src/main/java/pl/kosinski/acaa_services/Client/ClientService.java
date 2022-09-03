8581e7692d254edf7a252340bf4a4f1fe452d82b
Ivoriel <tkivor@gmail.com>
2022-09-03 13:50:16 +0200

    ClientService moved to the ACAA-services library.

---
+++ src/main/java/pl/kosinski/acaa_services/Client/ClientService.java
@@ -0,0 +1,11 @@
+package pl.kosinski.acaa_services.Client;
+
+import pl.kosinski.acaa_model.Client.Client;
+
+public interface ClientService {
+
+    Client save(Client client);
+
+    Client get(Long id);
+
+}
