f37fe4c1836603543deedef6f6b6dfdb5f96f237
Ivoriel <tkivor@gmail.com>
2022-09-04 09:12:05 +0200

    ClientServiceImpl moved to the ACAA-services library.

---
+++ src/main/java/pl/kosinski/acaa_services/Client/ClientServiceImpl.java
@@ -0,0 +1,22 @@
+package pl.kosinski.acaa_services.Client;
+
+import pl.kosinski.acaa_dao.Client.ClientDao;
+import pl.kosinski.acaa_model.Client.Client;
+
+public class ClientServiceImpl implements ClientService {
+
+    @Override
+    public Client save(Client client) {
+        return null;
+    }
+
+    @Override
+    public Client get(Long id) {
+        return null;
+    }
+
+    private Client toDto(ClientDao clientDao) {
+        return null;
+    }
+
+}
