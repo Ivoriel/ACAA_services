794d8d9df3b254782237ea933f33268ab1508e3c
Ivoriel <tkivor@gmail.com>
2022-09-06 10:10:58 +0200

    CompanyServiceImpl moved to the ACAA-services library.

---
+++ src/main/java/pl/kosinski/acaa_services/Company/CompanyServiceImpl.java
@@ -0,0 +1,20 @@
+package pl.kosinski.acaa_services.Company;
+
+import pl.kosinski.acaa_model.Company.Company;
+
+public class CompanyServiceImpl implements CompanyService {
+
+    @Override
+    public Company save(Company company) {
+        return null;
+    }
+
+    @Override
+    public Company get(Long id) {
+        return null;
+    }
+
+    private Company toDto() {
+        return null;
+    }
+}
