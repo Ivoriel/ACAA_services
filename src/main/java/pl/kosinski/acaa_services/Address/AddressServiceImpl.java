d3c832c0987dfa478d8e10df2f5754d027eef364
Ivoriel <tkivor@gmail.com>
2022-10-15 17:44:57 +0200

    toDto method added.

--- src/main/java/pl/kosinski/acaa_services/Address/AddressServiceImpl.java
+++ src/main/java/pl/kosinski/acaa_services/Address/AddressServiceImpl.java
@@ -1,7 +1,30 @@
 package pl.kosinski.acaa_services.Address;
 
 import org.springframework.stereotype.Component;
+import pl.kosinski.acaa_dao.Address.AddressDao;
+import pl.kosinski.acaa_dao.Address.AddressRepository;
+import pl.kosinski.acaa_dto.AddressDto;
 
 @Component
 public class AddressServiceImpl implements AddressService{
+
+    AddressRepository addressRepository;
+
+    @Override
+    public AddressDto getAddress(long id) {
+        return null;
+    }
+
+    private AddressDto toDto(AddressDao addressDao) {
+        AddressDto addressDto = new AddressDto();
+        addressDto.setId(addressDao.getId());
+        addressDto.setCountry(addressDao.getCountry());
+        addressDto.setMunicipality(addressDao.getMunicipality());
+        addressDto.setRegion(addressDao.getRegion());
+        addressDto.setZipCode(addressDao.getZipCode());
+        addressDto.setStreet(addressDao.getStreet());
+        addressDto.setBuildingNumber(addressDao.getBuildingNumber());
+        addressDto.setAdditionalIdentifier(addressDao.getAdditionalIdentifier());
+        return addressDto;
+    }
 }
