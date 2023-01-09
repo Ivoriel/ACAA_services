db444fd3582905c4c5bf339390e91606203588e1
Ivoriel <tkivor@gmail.com>
2023-01-09 01:01:54 +0100

    Returned value changed to ClientDao

--- src/main/java/pl/kosinski/acaa_services/Client/ClientService.java
+++ src/main/java/pl/kosinski/acaa_services/Client/ClientService.java
@@ -1,13 +1,14 @@
 package pl.kosinski.acaa_services.Client;
 
 import pl.kosinski.acaa_dao.Client.ClientDao;
+import pl.kosinski.acaa_dto.ClientDto;
 import pl.kosinski.acaa_model.Client.Client;
 
 public interface ClientService {
 
-    Client save(ClientDao clientDao);
+    ClientDto save(ClientDao clientDao);
 
-    Client get(Long id);
+    ClientDto get(Long id);
 
     boolean delete(Long id);
 
