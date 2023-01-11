c804e3630972cb728586b112a5cf957465623217
Ivoriel <tkivor@gmail.com>
2023-01-12 00:26:00 +0100

    Changed returns to dto

--- src/main/java/pl/kosinski/acaa_services/Company/CompanyService.java
+++ src/main/java/pl/kosinski/acaa_services/Company/CompanyService.java
@@ -1,13 +1,14 @@
 package pl.kosinski.acaa_services.Company;
 
 import pl.kosinski.acaa_dao.Company.CompanyDao;
+import pl.kosinski.acaa_dto.CompanyDto;
 import pl.kosinski.acaa_model.Company.Company;
 
 public interface CompanyService {
 
-    Company save(CompanyDao companyDao);
+    CompanyDto save(CompanyDao companyDao);
 
-    Company get(Long id);
+    CompanyDto get(Long id);
 
     boolean delete(Long id);
 
