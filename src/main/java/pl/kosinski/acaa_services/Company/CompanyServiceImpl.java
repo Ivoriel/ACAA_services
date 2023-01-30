d72f172eafde73ec891c8dd8c5b41310de0c5e22
Ivoriel <tkivor@gmail.com>
2023-01-30 15:35:21 +0100

    Implemented size method

--- src/main/java/pl/kosinski/acaa_services/Company/CompanyServiceImpl.java
+++ src/main/java/pl/kosinski/acaa_services/Company/CompanyServiceImpl.java
@@ -1,7 +1,6 @@
 package pl.kosinski.acaa_services.Company;
 
 import org.springframework.stereotype.Component;
-import pl.kosinski.acaa_dao.Client.ClientDao;
 import pl.kosinski.acaa_dao.Company.CompanyDao;
 import pl.kosinski.acaa_dao.Company.CompanyRepository;
 import pl.kosinski.acaa_dto.CompanyDto;
@@ -15,11 +14,13 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public CompanyDto save(CompanyDao companyDao) {
-        Optional<ClientDao> addressDaoOptional = clientRepository.get(clientDao.getId());
+        Optional<CompanyDao> addressDaoOptional = repository.get(companyDao.getId());
         if (Optional.ofNullable(addressDaoOptional).isPresent()) {
-            return toDto(addressDaoOptional.get().edit(clientDao.getName(), clientDao.getAddressId()));
+            return toDto(addressDaoOptional.get().edit(companyDao.getName(), companyDao.getClientId(),
+                    companyDao.getAddressId()));
         } else {
-            return toDto(new ClientDao(clientRepository.size(), clientDao.getName(), clientDao.getAddressId()));
+            return toDto(new CompanyDao(repository.size(), companyDao.getName(), companyDao.getClientId(),
+                    companyDao.getAddressId()));
         }
     }
 
@@ -37,7 +38,7 @@ public class CompanyServiceImpl implements CompanyService {
         CompanyDto dto = new CompanyDto();
         dto.setId(companyDao.getId());
         dto.setName(companyDao.getName());
-        dto.setClientId(companyDao.getClient());
+        dto.setClientId(companyDao.getClientId());
         dto.setAddressId(companyDao.getAddressId());
         return dto;
     }
