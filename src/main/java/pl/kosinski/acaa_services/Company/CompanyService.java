ac41cefa87b29b31a1ee39f0c073125f1ee67114
Ivoriel <tkivor@gmail.com>
2023-02-05 03:45:51 +0100

    Delete method return changed to void

--- src/main/java/pl/kosinski/acaa_services/Company/CompanyService.java
+++ src/main/java/pl/kosinski/acaa_services/Company/CompanyService.java
@@ -5,10 +5,10 @@ import pl.kosinski.acaa_dto.CompanyDto;
 
 public interface CompanyService {
 
-    CompanyDto save(CompanyDao companyDao);
+    CompanyDto save(CompanyDto companyDto);
 
     CompanyDto get(Long id);
 
-    boolean delete(Long id);
+    void delete(Long id);
 
 }
